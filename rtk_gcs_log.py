2b82cb4042faa138e74416c9605c3475e9ae5760
LiTangqing <e0012726@u.nus.edu>
2017-02-09 12:40:20 +0800

    added implementation to plot the result

--- rtk_gcs_log.py
+++ rtk_gcs_log.py
@@ -1,25 +1,38 @@
 import socket  
-from time import gmtime, strftime             
+from time import gmtime, strftime 
+import matplotlib.pyplot as plt   
+import time
 
+filename = raw_input(">>> image name: ") ## customise path here 
+filename = filename+ '.png'
+x =  []
+y =  [] 
 s = socket.socket()         
-port = 12345                
-ip = "192.168.420"
-s.connect(ip, port)
+port = 9000                
+ip = "192.168.42.1"  #replace with server's ip address
+s.connect((ip, port))
 print s.recv(1024)
-## write the incoming message to a file 
-with open('received_file.txt', 'wb') as f:
-    print ('file opened')
-    while True:
+
+i = 1
+a = time.time()
+
+while True:
+    try:
         print('receiving data...')
         data = s.recv(1024)
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime())+': data=%s',(data))      
+        b = time.time()
+        y.append(b-a)
+        x.append(i)
+        a = b 
+        i += 1    
         if not data:
-        	print("NO DATA RECEIVED")
-        	break
-        # write data to a file
-        f.write(data)
-
-print('Successfully get the file')
-
+            print("NO DATA RECEIVED")
+            break
+        print(strftime("%Y-%m-%d %H:%M:%S", gmtime())) 
+    except KeyboardInterrupt:   ##windows use Ctrl+c 
+        break
 s.close()    
- 
+
+plt.plot(x,y)
+plt.savefig(filename)
+plt.show()
